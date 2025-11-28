3889b09263a582f8c73895748f67f1efc651fc67
jz8344 <jose.zanabria.23e@utzmg.edu.mx>
2025-11-27 21:43:39 -0600

    Fix: Add csrf_exempt to status endpoint

--- trailynsafe/trailynapp/views.py
+++ trailynsafe/trailynapp/views.py
@@ -2,8 +2,10 @@
 Vista simple para endpoint de status
 """
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def status_check(request):
     """Endpoint de status para Railway"""
     return JsonResponse({
